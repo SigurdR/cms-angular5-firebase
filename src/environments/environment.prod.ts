1708282050ffad1327ac7208410120c7ec04bfea
Amada <Sigurd@Amadas-MacBook-Pro.local>
2018-11-28 23:50:09 +0800

    Complete Lecture 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4iVS6_j5yIaKRmgguiHVhAUGfA-4WHbU",
+    authDomain: "cms-database-3c878.firebaseapp.com",
+    databaseURL: "https://cms-database-3c878.firebaseio.com",
+    projectId: "cms-database-3c878",
+    storageBucket: "cms-database-3c878.appspot.com",
+    messagingSenderId: "775366702920"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4iVS6_j5yIaKRmgguiHVhAUGfA-4WHbU",
+    authDomain: "cms-database-3c878.firebaseapp.com",
+    databaseURL: "https://cms-database-3c878.firebaseio.com",
+    projectId: "cms-database-3c878",
+    storageBucket: "cms-database-3c878.appspot.com",
+    messagingSenderId: "775366702920"
+  }
 };
