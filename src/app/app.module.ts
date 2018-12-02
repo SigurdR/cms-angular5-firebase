d34fc1ccb1ff396827e12ab8616a5c15714b8805
Amada <Sigurd@Amadas-MacBook-Pro.local>
2018-12-02 17:51:40 +0800

    Add Login using Google Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { PagesListComponent } from './pages-list/pages-list.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AppRoutingModule } from './app-routing-module';
+import { AfService } from './providers/af.service';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesListComponent
+    PagesListComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AppRoutingModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AfService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
